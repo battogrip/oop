dadc5d5ba0c69996c3e156ce8af140343a52c095
battogrip <geocc123@yahoo.com>
2017-07-29 12:12:02 +0200

    java tut1 and 3, inline

---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		int myInt = 2;
+		String text = "Hello";
+		String blank = " ";
+		String name = "Gino";
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+		System.out.println("Hello" + " " + "Imo");
+		System.out.println("My integer is: " + myInt);
+		
+	}
+}
